2b7490693edb479814e0b974436c1c2b408c6e74
CosminPerRam <cosmin.p@live.com>
2024-02-11 00:50:21 +0200

    feat: set discord social link invite

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -84,7 +84,7 @@ export default defineConfig({
     },
     socialLinks: [
       { icon: "github", link: "https://github.com/gamedig" },
-      { icon: "discord", link: "https://discord.gg" },
+      { icon: "discord", link: "https://discord.gg/aEFPRkm984" },
     ],
     footer: {
       message: "Made with ❤️",
