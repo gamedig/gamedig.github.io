afa7fb951ac7cc4fe858241d590f7ea67996b72d
Cain <75994858+cainthebest@users.noreply.github.com>
2024-02-14 21:03:25 +0000

    fix: contributor component auto scroll

--- .vitepress/theme/components/HorizontalTeamMembers.vue
+++ .vitepress/theme/components/HorizontalTeamMembers.vue
@@ -1,27 +1,68 @@
 <script setup lang="ts">
 // Based on https://github.com/vuejs/vitepress/blob/20511006dba516ca8c06ed1dd3516547af668a0e/src/client/theme-default/components/VPTeamMembers.vue
+  
+import { onMounted, onUnmounted, ref, nextTick, Ref } from "vue";
+import type { DefaultTheme } from "vitepress/theme";
+import { VPTeamMembers } from "vitepress/theme";
 
-import type { DefaultTheme } from 'vitepress/theme'
-import { VPTeamMembers } from "vitepress/theme"
+interface VPTeamMembersComponent {
+  $el: HTMLElement;
+}
 
 interface Props {
-  size?: 'small' | 'medium'
-  members: DefaultTheme.TeamMember[]
+  size?: "small" | "medium";
+  members: DefaultTheme.TeamMember[];
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  size: 'medium'
-})
+  size: "medium",
+});
+
+const componentRef: Ref<VPTeamMembersComponent | null> = ref(null);
+
+onMounted(async () => {
+  await nextTick();
+
+  const component = componentRef.value;
+  if (!component || !(component.$el instanceof HTMLElement)) return;
+
+  const scrollContainer = component.$el.querySelector(
+    ".container"
+  ) as HTMLElement | null;
+  if (!scrollContainer) return;
+
+  let scrollAmount = 1;
+  const scrollFunction = () => {
+    if (scrollContainer.scrollWidth <= scrollContainer.clientWidth) return;
+
+    scrollContainer.scrollLeft += scrollAmount;
+    if (
+      scrollContainer.scrollLeft >=
+        scrollContainer.scrollWidth - scrollContainer.clientWidth ||
+      scrollContainer.scrollLeft <= 0
+    ) {
+      scrollAmount = -scrollAmount;
+    }
+  };
+
+  const interval = setInterval(scrollFunction, 20);
+
+  onUnmounted(() => {
+    clearInterval(interval);
+  });
+});
 </script>
 
 <template>
+  <div class="horizontal-team-members-wrapper">
     <VPTeamMembers
-        :size="size"
-        :members="members"
-        class="horizontal-team-members"
+      ref="componentRef"
+      :size="size"
+      :members="members"
+      class="horizontal-team-members"
     />
+  </div>
 </template>
-
 <style>
 .horizontal-team-members.VPTeamMembers.small .container .item {
   flex: 1 0 224px;
@@ -32,7 +73,7 @@ const props = withDefaults(defineProps<Props>(), {
 }
 
 @media (min-width: 375px) {
-    .horizontal-team-members.VPTeamMembers.medium .container .item  {
+  .horizontal-team-members.VPTeamMembers.medium .container .item {
     flex: 1 0 288px;
   }
 }
