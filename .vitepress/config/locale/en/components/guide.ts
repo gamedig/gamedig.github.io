6c59cd3922e81cd62d5808277c8093bb4578c1ae
Cain <75994858+cainthebest@users.noreply.github.com>
2024-03-03 03:15:52 +0000

    impl(guide): nav bar

---
+++ .vitepress/config/locale/en/components/guide.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'vitepress';
+
+export const guideNav: DefaultTheme.NavItemWithLink[] = [
+    { text: 'Getting Started', link: '/guide/' },
+    { text: 'Advantages of Using GameDig', link: '/guide/advantages' },
+    { text: 'Common Use Cases', link: '/guide/use-cases' },
+    { text: 'How to Contribute', link: '/guide/contributing' },
+    { text: 'Key Features Across Libraries', link: '/guide/key-features' },
+    { text: 'Troubleshooting Common Issues', link: '/guide/troubleshooting' },
+];
