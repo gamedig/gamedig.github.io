aa05803e289a9265901c5262b5c940bde720587b
Cain <75994858+cainthebest@users.noreply.github.com>
2024-03-03 02:58:39 +0000

    chore(uno config): remove unneeded comments and format

--- uno.config.ts
+++ uno.config.ts
@@ -1,25 +1,12 @@
-import { 
-  defineConfig, 
-  presetAttributify, 
-  presetIcons, 
-  presetUno, 
-  transformerDirectives 
-} from 'unocss' 
+import {
+    defineConfig,
+    presetUno,
+    presetIcons,
+    presetAttributify,
+    transformerDirectives,
+} from 'unocss';
 
 export default defineConfig({
-
-  presets: [
-    // presetUno enables UnoCSS preset, which is a complete, customizable utility-first CSS framework.
-    presetUno(),
-    // presetIcons enables support for Iconify icons, allowing the use of thousands of icons from various icon sets.
-    presetIcons(),
-    // presetAttributify enables Attributify Mode in UnoCSS, a mode which allows most utilities to be applied directly using HTML attributes.
-    presetAttributify(),
-  ],
-  
-  // transformers are used to apply transformations to the CSS.
-  transformers: [
-    // transformerDirectives enables the directives transformer, which can apply directives like v-show, v-hide.
-    transformerDirectives(),
-  ],
-})
+    presets: [presetUno(), presetIcons(), presetAttributify()],
+    transformers: [transformerDirectives()],
+});
